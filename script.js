c13efa3261dd8362d7763be69eeb2b1bc3e7f004
Dinesh kumar <dinneshkumar9@gmail.com>
2023-04-20 14:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,102 +1,64 @@
-//your JS code here. If required.
 //DOM elements
       let input = document.querySelector("#ip");
       const button = document.querySelector("#btn");
       let output = document.querySelector("#output");
 
-      //function getNum promise
-      function getNumber() {
-        return new Promise((resolve, reject) => {
+      //function promise chaining
+      function PromiseChaining() {
+        const myPromise = new Promise((res) => {
           setTimeout(() => {
-            const number = input.value;
-            resolve(number);
-            // console.log(`Input Num : ${number}`);
+            res(input.value);
           }, 2000);
         });
-      }
-      // function numMultiplyByTwo promise
-      function numMultiplyByTwo() {
-        return getNumber().then((number) => {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              const numDouble = number * 2;
-              resolve(numDouble);
-              // console.log(`Multiply Num : ${numDouble}`);
-            }, 1000);
-          });
-        });
-      }
-
-      // function result - 3 = ....
-      function numSubByThree() {
-        return numMultiplyByTwo().then((numDouble) => {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              const numMinus = numDouble - 3;
-              resolve(numMinus);
-              // console.log(`Minus Num : ${numMinus}`);
-            }, 1000);
-          });
-        });
-      }
-
-      // function for division of number
-      function numDivideByTwo() {
-        return numSubByThree().then((numMinus) => {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              // const numDiv = Math.floor(numMinus / 2);
-              const numDiv = numMinus / 2;
-              resolve(numDiv);
-              // console.log(`Divide Num : ${numDiv}`);
-            }, 1000);
-          });
-        });
-      }
-
-      // function to add 10 to result
-      function addNumberTen() {
-        return numDivideByTwo().then((numDiv) => {
-          return new Promise((resolve, reject) => {
-            setTimeout(() => {
-              const addNum = numDiv + 10;
-              resolve(addNum);
-              // console.log(`Added num : ${addNum}`);
-            }, 1000);
-          });
-        });
-      }
-      // handle promises
-      function handlePromises() {
-        getNumber()
+        myPromise
           .then((result) => {
-            output.innerText = `Result: ${result}`;
-            // console.log("Promise 1");
-            return numMultiplyByTwo();
+            output.textContent = `Result: ${result}`;
+            return result;
           })
           .then((result) => {
-            output.innerText = `Result: ${result}`;
-            // console.log("Promise 2");
-            return numSubByThree();
+            return new Promise((res) => {
+              setTimeout(() => {
+                res(result * 2);
+              }, 1000);
+            });
           })
           .then((result) => {
-            output.innerText = `Result: ${result}`;
-            // console.log("Promise 3");
-            return numDivideByTwo();
+            output.textContent = `Result: ${result}`;
+            return result;
           })
           .then((result) => {
-            output.innerText = `Result: ${result}`;
-            // console.log("Promise 4");
-            return addNumberTen();
+            return new Promise((res) => {
+              setTimeout(() => {
+                res(result - 3);
+              }, 1000);
+            });
           })
           .then((result) => {
-            output.innerText = `Final Result: ${result}`;
-            // console.log("promise COMPLeted");
+            output.textContent = `Result: ${result}`;
+            return result;
           })
-          .catch((err) => {
-            console.log(err);
+          .then((result) => {
+            return new Promise((res) => {
+              setTimeout(() => {
+                res(result / 2);
+              }, 1000);
+            });
+          })
+          .then((result) => {
+            output.textContent = `Result: ${result}`;
+            return result;
+          })
+          .then((result) => {
+            return new Promise((res) => {
+              setTimeout(() => {
+                res(result + 10);
+              }, 1000);
+            });
+          })
+          .then((result) => {
+            output.textContent = `Final Result: ${result}`;
           });
       }
 
       // click event
-      button.addEventListener("click", handlePromises);
+      button.addEventListener("click", PromiseChaining);
